2a61f4fe4ff874afcaa77820e9aff3996dee7931
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-01-15 16:40:06 +0100

    Add parser script

---
+++ phosphorylation-example/phosphorylation_parser_args.py
@@ -0,0 +1,52 @@
+import argparse
+from pathlib import Path
+
+
+def add_phosphorylation_arguments(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        "--mesh-folder",
+        type=Path,
+        default=Path.cwd().parent / "meshes-phosphorylation" / "DemoSphere.h5",
+    )
+    parser.add_argument(
+        "-o", "--outdir", type=Path, default=Path("results_phosphorylation")
+    )
+    parser.add_argument(
+        "-dt",
+        "--time-step",
+        type=float,
+        default=0.01,
+    )
+    parser.add_argument(
+        "--curRadius",
+        type=float,
+        default=1.0,
+    )
+    parser.add_argument(
+        "--axisymmetric",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--no-enforce-mass-conservation",
+        action="store_true",
+        default=False,
+    )
+
+
+def add_preprocess_phosphorylation_mesh_arguments(
+    parser: argparse.ArgumentParser,
+) -> None:
+    parser.add_argument(
+        "--mesh-folder",
+        type=Path,
+        default=Path.cwd().parent / "meshes-phosphorylation",
+    )
+    parser.add_argument("--curRadius", type=float, default=1.0)
+    parser.add_argument("--hEdge", type=float, default=0.2)
+    parser.add_argument("--num-refinements", type=int, default=0)
+    parser.add_argument(
+        "--axisymmetric",
+        action="store_true",
+        default=False,
+    )
