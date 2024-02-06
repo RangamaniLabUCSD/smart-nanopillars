1bc058745458bf22329d4d89e374946c09163471
Henrik Nicolay Finsberg <henriknf@simula.no>
2024-02-06 21:14:10 +0100

    Remove --num-refinement arg from run script

--- scripts/run_phosphorylation.sh
+++ scripts/run_phosphorylation.sh
@@ -3,7 +3,7 @@
 # do
 #     for refinement in 0 1 2
 #     do
-#         python3 main.py phosphorylation-preprocess --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
+#         python3 main.py phosphorylation-preprocess --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
 #     done
 # done
 
@@ -21,9 +21,9 @@ for radius in 1 2 4 6 8 10
 do
     for refinement in 0 1 2
     do
-        for diffusion in 0.01 0.1 1.0 10.0 100.0
+        for diffusion in 0.01 0.1 1.0 100.0
         do
-            python3 main.py --submit-ex3 phosphorylation --diffusion $diffusion --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
+            python3 main.py --submit-ex3 phosphorylation --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
         done
     done
 done
@@ -35,7 +35,7 @@ do
     do
         for diffusion in 0.01 0.1 1.0 10.0 100.0
         do
-            python3 main.py --submit-ex3 phosphorylation --axisymmetric --diffusion $diffusion --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}"
+            python3 main.py --submit-ex3 phosphorylation --axisymmetric --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}"
         done
     done
 done
