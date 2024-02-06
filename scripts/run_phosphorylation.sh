c5554d2892aa606f7bb61ca932e273792dca7165
Henrik Nicolay Finsberg <henriknf@simula.no>
2024-02-06 20:33:05 +0100

    Remove dry-run and add axisymmetric

--- scripts/run_phosphorylation.sh
+++ scripts/run_phosphorylation.sh
@@ -3,27 +3,48 @@
 # do
 #     for refinement in 0 1 2
 #     do
-#         python3 main.py --dry-run phosphorylation-preprocess --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
+#         python3 main.py phosphorylation-preprocess --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
 #     done
 # done
 
-# Diffusion and Refinements
+# # Create axisymmetric meshes
+# for radius in 1.0 2.0 4.0 6.0 8.0 10.0
+# do
+#     for refinement in 0 1 2
+#     do
+#         python3 main.py phosphorylation-preprocess --axisymmetric --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}"
+#     done
+# done
+
+# Diffusion, Refinements
 for radius in 1.0 2.0 4.0 6.0 8.0 10.0
 do
     for refinement in 0 1 2
     do
         for diffusion in 0.01, 0.1, 1, 10, 100
         do
-            python3 main.py --dry-run phosphorylation-preprocess --diffusion $diffusion --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
+            python3 main.py phosphorylation --diffusion $diffusion --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
         done
     done
 done
 
-# Time steps
+#Same with axissymetric
+for radius in 1.0 2.0 4.0 6.0 8.0 10.0
+do
+    for refinement in 0 1 2
+    do
+        for diffusion in 0.01, 0.1, 1, 10, 100
+        do
+            python3 main.py phosphorylation --axisymmetric --diffusion $diffusion --curRadius $radius --num-refinements $refinement --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}"
+        done
+    done
+done
+
+# Time step
 for radius in 1.0 2.0 4.0 6.0 8.0 10.0
 do
     for timestep in 0.005 0.0025 .001
     do
-        python3 main.py --dry-run phosphorylation-preprocess --time-step $timestep --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_0"
+        python3 main.py phosphorylation --time-step $timestep --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_0"
     done
 done
