d3cfcef3a6101924bde2038c67693fed99167229
Henrik Nicolay Finsberg <henriknf@simula.no>
2024-03-07 10:16:18 +0100

    Specify quadrature degree and add jobs running example 3 in parallel to job script

--- scripts/run_phosphorylation.sh
+++ scripts/run_phosphorylation.sh
@@ -7,7 +7,7 @@ do
     done
 done
 
-# # Create axisymmetric meshes
+# Create axisymmetric meshes
 for radius in 1 2 4 6 8 10
 do
     for refinement in 0 1 2
@@ -16,6 +16,8 @@ do
     done
 done
 
+partition=defq
+
 # Diffusion, Refinements
 for radius in 1 2 4 6 8 10
 do
@@ -23,7 +25,7 @@ do
     do
         for diffusion in 0.01 0.1 1.0 10.0 100.0
         do
-            python3 main.py --submit-ex3 phosphorylation --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
+            python3 main.py --partition=$partition --submit-ex3 phosphorylation --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
         done
     done
 done
@@ -35,16 +37,13 @@ do
     do
         for diffusion in 0.01 0.1 1.0 10.0 100.0
         do
-            python3 main.py --submit-ex3 phosphorylation --axisymmetric --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}"
+            python3 main.py --partition=$partition --submit-ex3 phosphorylation --axisymmetric --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}"
         done
     done
 done
 
-# Time step
-# for radius in 1 2 4 6 8 10
-# do
-#     for timestep in 0.005 0.0025 .001
-#     do
-#         python3 main.py --submit-ex3 phosphorylation --time-step $timestep --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_0"
-#     done
-# done
+# Run with MPI for D = 1, Raidus = 10 and 2 levels of refinement
+for ntasks in 2 4 6 8 10 12 14 16
+do
+    python3 main.py --ntasks $ntasks --partition=$partition --submit-ex3 phosphorylation --diffusion 1.0 --curRadius 10 --mesh-folder "meshes-phosphorylation/curRadius_10_refined_2"
+done
