ade00a421f938d96e246e5863bf7e59cabb69b6b
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-03-08 13:42:22 +0100

    Fix compbinations for mechanotransduction

--- scripts/run_mechanotransduction.sh
+++ scripts/run_mechanotransduction.sh
@@ -1,18 +1,32 @@
 # Create meshes
-python3 main.py preprocess-mech-mesh --shape circle --num-refinements 0 --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_0
-python3 main.py preprocess-mech-mesh --shape circle --num-refinements 1 --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_1
-python3 main.py preprocess-mech-mesh --shape circle --num-refinements 2 --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_2
-python3 main.py preprocess-mech-mesh --shape circle --num-refinements 3 --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_3
+for shape in circle star rect
+do
+    for refinement in 0 1 2 3
+    do
+        python3 main.py mechanotransduction-preprocess --shape $shape --num-refinements $refinement --mesh-folder meshes-mechanotransduction/${shape}_hEdge_0.6_refined_${refinement}
+    done
+done
 
 # Base case
-python3 main.py --submit-ex3 mechanotransduction --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_0 --time-step 0.01 --e-val 70000000 --axisymmetric
+for eval in 0.1 5.7 70000000
+do
+    for shape in circle star rect
+    do
+        for refinement in 0 1 2 3
+        do
+            python3 main.py mechanotransduction --mesh-folder meshes-mechanotransduction/${shape}_hEdge_0.6_refined_${refinement} --time-step 0.01 --e-val $eval --axisymmetric -o results-mechanotransduction/${shape}_hEdge_0.6_refined_${refinement}_e_${eval}
+        done
+    done
+done
 
-# Run spatial convergence
-python3 main.py --submit-ex3 mechanotransduction --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_1 --time-step 0.01 --e-val 70000000 --axisymmetric
-python3 main.py --submit-ex3 mechanotransduction --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_2 --time-step 0.01 --e-val 70000000 --axisymmetric
-python3 main.py --submit-ex3 mechanotransduction --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_3 --time-step 0.01 --e-val 70000000 --axisymmetric
-
-# # Run temporal convergence
-python3 main.py --submit-ex3 mechanotransduction --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_0 --time-step 0.02 --e-val 70000000 --axisymmetric
-python3 main.py --submit-ex3 mechanotransduction --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_0 --time-step 0.005 --e-val 70000000 --axisymmetric
-python3 main.py --submit-ex3 mechanotransduction --mesh-folder meshes-mechanotransduction/circle_hEdge_0.6_refined_0 --time-step 0.001 --e-val 70000000 --axisymmetric
+# Well mixed case
+for eval in 0.1 5.7 70000000
+do
+    for shape in circle star rect
+    do
+        for refinement in 0 1 2 3
+        do
+            python3 main.py --dry-run mechanotransduction --mesh-folder meshes-mechanotransduction/${shape}_hEdge_0.6_refined_${refinement} --time-step 0.01 --e-val $eval --axisymmetric --well-mixed -o results-mechanotransduction/${shape}_hEdge_0.6_refined_${refinement}_e_${eval}_well-mixed
+        done
+    done
+done
