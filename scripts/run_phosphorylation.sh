4d5a6e59e1ceced82fa7b286f980657e377441f0
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-03-08 12:39:23 +0100

    Add output folder in bash script

--- scripts/run_phosphorylation.sh
+++ scripts/run_phosphorylation.sh
@@ -25,7 +25,7 @@ do
     do
         for diffusion in 0.01 0.1 1.0 10.0 100.0
         do
-            python3 main.py --partition=$partition  phosphorylation --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}"
+            python3 main.py --partition=$partition  phosphorylation --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_refined_${refinement}" -o "results-phosphorylation/curRadius_${radius}_refined_${refinement}_diffusion_${diffusion}"
         done
     done
 done
@@ -37,7 +37,7 @@ do
     do
         for diffusion in 0.01 0.1 1.0 10.0 100.0
         do
-            python3 main.py --partition=$partition  phosphorylation --axisymmetric --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}"
+            python3 main.py --partition=$partition  phosphorylation --axisymmetric --diffusion $diffusion --curRadius $radius --mesh-folder "meshes-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}" -o "results-phosphorylation/curRadius_${radius}_axisymmetric_refined_${refinement}_diffusion_${diffusion}"
         done
     done
 done
@@ -45,5 +45,5 @@ done
 # Run with MPI for D = 1, Raidus = 10 and 2 levels of refinement
 for ntasks in 2 4 6 8 10 12 14 16
 do
-    python3 main.py --ntasks $ntasks --partition=$partition  phosphorylation --diffusion 1.0 --curRadius 10 --mesh-folder "meshes-phosphorylation/curRadius_10_refined_2"
+    python3 main.py --ntasks $ntasks --partition=$partition  phosphorylation --diffusion 1.0 --curRadius 10 --mesh-folder "meshes-phosphorylation/curRadius_10_refined_2" -o "results-phosphorylation/curRadius_10_refined_2_diffusion_1.0_ntasks_${ntasks}"
 done
