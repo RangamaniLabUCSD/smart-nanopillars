2daa939db13345a9b0ad7ef1edbda197ae911abc
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-04-24 10:25:48 +0200

    Fix run script so that it actually runs in parallel

--- scripts/runner.py
+++ scripts/runner.py
@@ -70,7 +70,7 @@ def run(
     args,
     dry_run: bool,
     script: str,
-    submit_ex3: bool, # FIXME: Change this and next param to enum
+    submit_ex3: bool,  # FIXME: Change this and next param to enum
     submit_tscc: bool,
     job_name: str = "",
     ntasks: int = 1,
@@ -87,10 +87,7 @@ def run(
     elif submit_tscc:
         template = tscc_template
     else:
-        if ntasks > 1:
-           sp.run([sys.executable, script, *args])
-        else:
-            sp.run(["mpirun", "-n", str(ntasks), sys.executable, script, *in_args])
+        sp.run(["mpirun", "-n", str(ntasks), sys.executable, script, *in_args])
         return
 
     job_file = Path("tmp_job.sbatch")
@@ -129,7 +126,7 @@ def preprocess_phosphorylation_mesh(
     ]
     if axisymmetric:
         args.append("--axisymmetric")
-    
+
     if rect:
         args.append("--rect")
 
@@ -185,13 +182,13 @@ def phosphorylation_example(
 
     if rect:
         args.append("--rect")
-    
+
     if write_checkpoint:
         args.append("--write-checkpoint")
 
     if submit_ex3 is False:
         args.extend(["--outdir", Path(outdir).as_posix()])
-        
+
     script = (
         (here / ".." / "phosphorylation-example" / "phosphorylation.py")
         .absolute()
@@ -448,7 +445,7 @@ def postprocess_mechanotransduction(
         args.append("--skip-if-processed")
     if use_tex:
         args.append("--use-tex")
-        
+
     script = (
         (here / ".." / "mechanotransduction-example" / "postprocess.py")
         .absolute()
@@ -518,12 +515,7 @@ def cru_example(
     if submit_ex3 is False:
         args.extend(["--outdir", Path(outdir).as_posix()])
 
-    script = (
-        (here / ".." / "cru-example" / "cru.py")
-        .absolute()
-        .resolve()
-        .as_posix()
-    )
+    script = (here / ".." / "cru-example" / "cru.py").absolute().resolve().as_posix()
     run(
         job_name="cru",
         args=args,
@@ -563,6 +555,7 @@ def preprocess_spine_mesh(
         submit_ex3=False,
         submit_tscc=False,
     )
+
 
 def dendritic_spine_postprocess(
     results_folder: Path,
