b952265b41c57ccdf749a92aac38eb28c369914e
Henrik Nicolay Finsberg <henriknf@simula.no>
2024-04-26 10:44:39 +0200

    Plot only one e-val

--- mechanotransduction-example/postprocess.py
+++ mechanotransduction-example/postprocess.py
@@ -4,12 +4,17 @@ from pathlib import Path
 import pandas as pd
 import json
 from typing import NamedTuple, Any
-from itertools import cycle
 import numpy as np
 import matplotlib.pyplot as plt
 
 import mech_parser_args
 from mechanotransduction_ode import mechanotransduction_ode_calc
+
+var_names_all = ["Cofilin_P", "Fak", "mDia", "LaminA", "FActin", "RhoA_GTP", "mDia_A", "NPC_A", "GActin", "NPC",
+        "ROCK_A", "Myo", "Cofilin_NP", "LaminA_p", "YAPTAZ_nuc", "pFak", "YAPTAZ_phos", "YAPTAZ", "RhoA_GDP", "LIMK",
+        "Myo_A", "ROCK", "Positionboolean", "LIMK_A", "MRTF", "MRTF_nuc"]
+ode_fac_idx = var_names_all.index("FActin")
+ode_yap_idx = var_names_all.index("YAPTAZ_nuc")
 
 
 class Data(NamedTuple):
@@ -33,7 +38,7 @@ class Data(NamedTuple):
     
     @property
     def well_mixed(self):
-        return self.config.get("well_mixed", False)
+        return self.config.get("well_mixed", True)
     
     @property
     def dt(self) -> float:
@@ -72,89 +77,57 @@ def load_all_data(main_path: Path):
     return all_data
 
 
-def plot_data(data: list[Data], output_folder, format: str = "png"):
-    fig, ax = plt.subplots(2, 3, sharex=True, sharey="row", figsize=(12, 8))
-    fig_t, ax_t = plt.subplots(1, 3, sharey=True, figsize=(12, 4))    
-    linestyles = [cycle(["-", "--", ":", "-."]) for _ in range(3)]
+def get_ode_solution(e_val):
+    nuc_vol = 70.6
+    nm_area = 58.5
+    Ac = 133
 
-    # Plot 
-    refinements = set()
-    e_vals = set()
-    z_cutoffs = set()
+    geoParam = [1925.03/4, nuc_vol, 1294.5/4, nm_area, Ac]
 
-    for d in data:
-        print(d.refinement, d.dt, d.e_val, d.z_cutoff, d.well_mixed)
-        refinements.add(d.refinement)
-        e_vals.add(d.e_val)
-        z_cutoffs.add(d.z_cutoff)
+    return mechanotransduction_ode_calc([0, 10000], e_val, geoParam)
 
-    e_vals = sorted(list(e_vals))
-    z_cutoffs = sorted(list(z_cutoffs))
-    refinements = sorted(list(refinements))
-   
+
+def plot_data(all_data: list[Data], output_folder, format: str = "png"):
+
+    data = [d for d in all_data if np.isclose(d.e_val, 70000000.0) and d.well_mixed]
+    refinements = sorted(set(d.refinement for d in data))
+
     # Eval vs Cutoff
     x = np.arange(len(refinements))
-    fig_yap, ax_yap = plt.subplots(len(e_vals), 2, sharex=True, sharey="row", figsize=(10, 10))
-    fig_fac, ax_fac = plt.subplots(len(e_vals), 2, sharex=True, sharey="row", figsize=(10, 10))
-    fig_t, ax_t = plt.subplots(len(e_vals), 2, sharex=True, sharey="row", figsize=(10, 10))
-    for i, e_val in enumerate(e_vals):
-        ax2 = ax_yap[i, -1].twinx()
-        ax2.set_yticks([])
-        ax2.set_ylabel(f"e val = {e_val}")
 
-        ax2 = ax_fac[i, -1].twinx()
-        ax2.set_yticks([])
-        ax2.set_ylabel(f"e val = {e_val}")
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    lines = []
+    labels = []
+    timings = []
+    for d in sorted(data, key=lambda x: x.refinement):
+        l, = ax[0].plot(d.t, d.yap, label=f"refinement = {d.refinement}")
+        ax[1].plot(d.t, d.fac, label=f"refinement = {d.refinement}")
+        lines.append(l)
+        labels.append(f"{d.refinement}")
 
-        ax2 = ax_t[i, -1].twinx()
-        ax2.set_yticks([])
-        ax2.set_ylabel(f"e val = {e_val}")
-        for j, well_mixed in enumerate([True, False]):
-            ax_yap[0, j].set_title(f"well mixed = {well_mixed}")
-            ax_fac[0, j].set_title(f"well mixed = {well_mixed}")
-            ax_t[0, j].set_title(f"well mixed = {well_mixed}")
-            times = []
-       
-            for d in sorted([di for di in data if di.e_val == e_val and di.well_mixed is well_mixed], key=lambda x: x.refinement):
-                times.append(d.total_run_time)
-                ax_yap[i, j].plot(d.t, d.yap, label=f"refinement = {d.refinement}")
-                ax_fac[i, j].plot(d.t, d.fac, label=f"refinement = {d.refinement}")
-            
-            if well_mixed:
-                nuc_vol = 70.6
-                nm_area = 58.5
-                Ac = 133
-                # geoParam = [cyto_vol, nuc_vol, pm_area, nm_area, Ac]
-                geoParam = [1925.03/4, nuc_vol, 1294.5/4, nm_area, Ac]
-                var_names_all = ["Cofilin_P", "Fak", "mDia", "LaminA", "FActin", "RhoA_GTP", "mDia_A", "NPC_A", "GActin", "NPC",
-                        "ROCK_A", "Myo", "Cofilin_NP", "LaminA_p", "YAPTAZ_nuc", "pFak", "YAPTAZ_phos", "YAPTAZ", "RhoA_GDP", "LIMK",
-                        "Myo_A", "ROCK", "Positionboolean", "LIMK_A", "MRTF", "MRTF_nuc"]
-                ode_fac_idx = var_names_all.index("FActin")
-                ode_yap_idx = var_names_all.index("YAPTAZ_nuc")
-                # ode_yap_idx = [var_names_all.index(name) for name in ["YAPTAZ_nuc", "YAPTAZ_phos", "YAPTAZ"]]
-                # plot ode solution
-                t_ode, ode_results = mechanotransduction_ode_calc([0, 10000], e_val, geoParam)
-                ax_yap[i, j].plot(t_ode, ode_results[:,ode_yap_idx],#/(ode_results[:,ode_yap_idx[1]]+ode_results[:,ode_yap_idx[2]]), 
-                                linestyle='dashed', label=f"ODE solution")
-                ax_fac[i, j].plot(t_ode, ode_results[:,ode_fac_idx], 
-                                linestyle='dashed', label=f"ODE solution")
+        timings.append(float(d.total_run_time))
 
-            ax_t[i, j].bar(x, times, width=0.3)
-            ax_t[i, j].set_yscale("log")
-            ax_t[i, j].set_xticks(x)
-            ax_t[i, j].set_xticklabels(list(sorted(refinements)))
-            if i == len(e_vals) - 1:
-                ax_t[i, j].set_xlabel("Refinement")
-
-            ax_yap[i, j].legend()
-            ax_fac[i, j].legend()
-            ax_t[i, j].legend()
-
-    fig_yap.savefig((Path(output_folder) / "yap").with_suffix(f".{format}"))
-    fig_fac.savefig((Path(output_folder) / "fac").with_suffix(f".{format}"))
-    fig_t.savefig((Path(output_folder) / "timings").with_suffix(f".{format}"))
+    # Plot ODE soltution
+    t_ode, ode_results = get_ode_solution(d.e_val)
+    ax[0].plot(t_ode, ode_results[:,ode_yap_idx], linestyle='dashed', label=f"ODE solution")
+    ax[1].plot(t_ode, ode_results[:,ode_fac_idx], linestyle='dashed', label=f"ODE solution")
     
-
+    ax[0].set_ylabel("YAP/TAZ")
+    ax[1].set_ylabel("F-Actin")
+    ax[1].set_xlabel("Time")
+    lgd = fig.legend(lines, labels, title="Refinement", loc="center right", bbox_to_anchor=(1.1, 0.5))
+    fig.subplots_adjust(right=0.99)
+    fig.savefig((output_folder / "results.png").with_suffix(f".{format}"), bbox_extra_artists=(lgd,), bbox_inches="tight")
+    
+    fig_t, ax_t = plt.subplots()    
+    ax_t.bar(x, timings)
+    # ax_t.set_yscale("log")
+    ax_t.set_xticks(x)
+    ax_t.set_xticklabels(refinements)
+    ax_t.set_xlabel("Refinement")
+    ax_t.set_ylabel("Total run time [s]")
+    ax_t.set_title("Total run time vs refinement")
+    fig_t.savefig((output_folder / "timings.png").with_suffix(f".{format}"))
         
 def load_timings(folder: Path) -> dict[str, Any]:
     timings = (folder / "timings.txt").read_text()
