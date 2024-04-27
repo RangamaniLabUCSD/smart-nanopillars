8bd03ec759e5bcf34c72b03643598f36bbaac4c1
emmetfrancis <emmet.a.francis@gmail.com>
2024-04-27 15:14:49 -0700

    Uncomment lines in bash script for mesh gen, delete extra python files

--- scripts/mesh_gen.sh
+++ scripts/mesh_gen.sh
@@ -1,15 +1,15 @@
 # echo "Saving mesh for mito example..."
-# python3 main.py mito-preprocess --input-mesh-file /root/shared/gitrepos/smart-comp-sci/meshes/mito1_coarser2_mesh.xml \
-# --output-mesh-file /root/scratch/write-meshes/mito/meshes/mito_mesh.h5 --input-curv-file /root/shared/gitrepos/smart-comp-sci/meshes/mito1_coarser2_curvature.xml \
-# --output-curv-file /root/scratch/write-meshes/mito/meshes/mito_curv.xdmf --single-compartment-im
+python3 main.py mito-preprocess --input-mesh-file /root/shared/gitrepos/smart-comp-sci/meshes/mito1_coarser2_mesh.xml \
+--output-mesh-file /root/scratch/write-meshes/mito/meshes/mito_mesh.h5 --input-curv-file /root/shared/gitrepos/smart-comp-sci/meshes/mito1_coarser2_curvature.xml \
+--output-curv-file /root/scratch/write-meshes/mito/meshes/mito_curv.xdmf --single-compartment-im
 
 # echo "Saving mesh for dendritic spine example..."
-# python3 main.py dendritic-spine-preprocess --input-mesh-file /root/shared/gitrepos/smart-comp-sci/meshes/1spine_PM10_PSD11_ERM12_cyto1_ER2_coarser.xml \
-# --output-mesh-file /root/scratch/write-meshes/dendritic_spine/meshes/spine_mesh.h5
+python3 main.py dendritic-spine-preprocess --input-mesh-file /root/shared/gitrepos/smart-comp-sci/meshes/1spine_PM10_PSD11_ERM12_cyto1_ER2_coarser.xml \
+--output-mesh-file /root/scratch/write-meshes/dendritic_spine/meshes/spine_mesh.h5
 
 # echo "Saving mesh for cru example..."
-# python3 main.py cru-preprocess --input-mesh-file /root/shared/gitrepos/smart-comp-sci/meshes/CRU_mesh.xml \
-# --output-mesh-file /root/scratch/write-meshes/cru/meshes/mito_mesh.h5
+python3 main.py cru-preprocess --input-mesh-file /root/shared/gitrepos/smart-comp-sci/meshes/CRU_mesh.xml \
+--output-mesh-file /root/scratch/write-meshes/cru/meshes/mito_mesh.h5
 
 echo "Saving meshes for mechanotransduction example"
 # Create meshes with symm considerations
