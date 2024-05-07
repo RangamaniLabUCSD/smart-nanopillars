bea29873039ed241fb832e4c12a5b09596544e13
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-05-07 16:29:54 +0200

    Add petsc profiling to dendritic spine example and plot these timings

--- dendritic-spine-example/postprocess.py
+++ dendritic-spine-example/postprocess.py
@@ -14,8 +14,10 @@ import dendritic_spine_args
 
 ntasks_pattern = re.compile("ntasks: (?P<n>\d+)")
 
+
 class Data(NamedTuple):
     timings_: list[dict[str, Any]]
+    petsc_timings_: list[dict[str, Any]]
     config: dict[str, Any]
     t: np.ndarray
     concVec: np.ndarray
@@ -28,30 +30,34 @@ class Data(NamedTuple):
     @property
     def mesh(self) -> str:
         return Path(self.config["mesh_file"]).stem
-    
-    
+
     @property
     def dt(self) -> float:
         return self.config["solver"]["initial_dt"]
-    
+
     @property
     def timings(self):
         return pd.DataFrame(self.timings_)
-    
+
+    @property
+    def petsc_timings(self):
+        return pd.DataFrame(self.petsc_timings_)
+
     @property
     def num_refinements(self) -> int:
         if "refined" in self.mesh:
             return int(self.mesh.split("_")[-1])
         return 0
-    
+
     @property
     def total_run_time(self) -> float:
-
         try:
-            return self.timings[self.timings["name"] == "dendritic-spine-example"]["wall tot"].values[0]
+            return self.timings[self.timings["name"] == "dendritic-spine-example"][
+                "wall tot"
+            ].values[0]
         except IndexError:
             return np.nan
-    
+
     def to_json(self) -> dict[str, Any]:
         return {
             "t": self.t.tolist(),
@@ -62,6 +68,7 @@ class Data(NamedTuple):
             "stderr": self.stderr,
             "stdout": self.stdout,
             "ntasks": self.ntasks,
+            "petsc_timings": self.petsc_timings_,
         }
 
 
@@ -87,10 +94,10 @@ def parse_timings(timings: str) -> dict[str, Any]:
     data.append(dict(zip(header, item)))
     return data
 
+
 def load_all_data(main_path: Path):
     all_data = []
     for folder in (f for f in Path(main_path).iterdir() if f.is_dir()):
-        
         try:
             data = load_data(folder=folder)
             print(data.mesh)
@@ -98,21 +105,25 @@ def load_all_data(main_path: Path):
         except FileNotFoundError as e:
             print(f"Skipping folder {folder}, due to {e}")
             continue
-       
+
         all_data.append(data)
     return all_data
 
 
 def plot_data(all_data: list[Data], output_folder, format: str = "png"):
-
     data = [d for d in all_data if d.ntasks == 1]
     fig, ax = plt.subplots(2, 4, sharex=True, sharey="row", figsize=(15, 8))
-      
-    mesh2index = {"1spine_mesh_coarser": 0, "1spine_mesh_coarser_refined_1": 1, "1spine_mesh_coarser_refined_2": 2,  "1spine_mesh": 3}
-    
+
+    mesh2index = {
+        "1spine_mesh_coarser": 0,
+        "1spine_mesh_coarser_refined_1": 1,
+        "1spine_mesh_coarser_refined_2": 2,
+        "1spine_mesh": 3,
+    }
+
     dts = list(sorted({d.dt for d in data}))
     dts2color = {d: c for d, c in zip(dts, cycle(plt.cm.tab10.colors))}
-    
+
     timings = [np.zeros_like(dts) for _ in mesh2index]
     lines = []
     labels = []
@@ -120,7 +131,7 @@ def plot_data(all_data: list[Data], output_folder, format: str = "png"):
         index = mesh2index[d.mesh]
         timings[index][dts.index(d.dt)] = d.total_run_time
 
-        l, = ax[0, index].plot(d.t, d.concVec, label=d.dt, color=dts2color[d.dt])
+        (l,) = ax[0, index].plot(d.t, d.concVec, label=d.dt, color=dts2color[d.dt])
         if index == 0:
             lines.append(l)
             labels.append(f"{d.dt:.2e}")
@@ -128,35 +139,53 @@ def plot_data(all_data: list[Data], output_folder, format: str = "png"):
         # breakpoint()
         print(d.mesh, d.dt, d.num_refinements, d.folder.stem)
 
-
     for k, v in mesh2index.items():
         ax[0, v].set_title(" ".join(k.split("_")))
-    
+
     ax[0, 0].set_ylabel("Average Cytosolic calcium (μM)")
     ax[1, 0].set_ylabel("Average Gradient of Cytosolic calcium (μM)")
 
-    lgd = fig.legend(lines, labels, title="Time step", loc="center right", bbox_to_anchor=(1.1, 0.5))
+    lgd = fig.legend(
+        lines, labels, title="Time step", loc="center right", bbox_to_anchor=(1.1, 0.5)
+    )
     fig.subplots_adjust(right=0.99)
-    fig.savefig((output_folder / "results.png").with_suffix(f".{format}"), bbox_extra_artists=(lgd,), bbox_inches="tight")
-
+    fig.savefig(
+        (output_folder / "results.png").with_suffix(f".{format}"),
+        bbox_extra_artists=(lgd,),
+        bbox_inches="tight",
+    )
 
     # Plot timings
     fig_t, ax_t = plt.subplots()
 
     x = np.arange(0, len(dts))
-    ax_t.bar(x-0.25, timings[0], width=0.25, label=" ".join(list(mesh2index.keys())[0].split("_")))
-    ax_t.bar(x, timings[1], width=0.25, label=" ".join(list(mesh2index.keys())[1].split("_")))
-    ax_t.bar(x+0.25, timings[2], width=0.25, label=" ".join(list(mesh2index.keys())[2].split("_")))
+    ax_t.bar(
+        x - 0.25,
+        timings[0],
+        width=0.25,
+        label=" ".join(list(mesh2index.keys())[0].split("_")),
+    )
+    ax_t.bar(
+        x, timings[1], width=0.25, label=" ".join(list(mesh2index.keys())[1].split("_"))
+    )
+    ax_t.bar(
+        x + 0.25,
+        timings[2],
+        width=0.25,
+        label=" ".join(list(mesh2index.keys())[2].split("_")),
+    )
     ax_t.set_yscale("log")
     ax_t.set_xticks(x)
     ax_t.set_xticklabels(dts)
     ax_t.legend()
     ax_t.grid()
     fig_t.savefig((output_folder / "timings.png").with_suffix(f".{format}"))
-   
+
 
 def plot_refinement_study(all_data: list[Data], output_folder, format: str = "png"):
-    data = sorted([d for d in all_data if "coarser" in d.mesh], key=lambda x: x.num_refinements)
+    data = sorted(
+        [d for d in all_data if "coarser" in d.mesh], key=lambda x: x.num_refinements
+    )
     dts = list(sorted({d.dt for d in data}))
     num_refinements = list(sorted({d.num_refinements for d in data}))
     # Find index where we have all refinements
@@ -173,7 +202,7 @@ def plot_refinement_study(all_data: list[Data], output_folder, format: str = "pn
         if key in plotted:
             continue
         plotted.add(key)
-        l, = ax[0, index].plot(d.t, d.concVec, label=d.num_refinements)
+        (l,) = ax[0, index].plot(d.t, d.concVec, label=d.num_refinements)
         ax[1, index].plot(d.t, d.gradVec, label=d.num_refinements)
         ax[0, index].set_title(f"dt = {d.dt}")
         if index == 3:
@@ -181,10 +210,21 @@ def plot_refinement_study(all_data: list[Data], output_folder, format: str = "pn
             labels.append(d.num_refinements)
     ax[0, 0].set_ylabel("Average Cytosolic calcium (μM)")
     ax[1, 0].set_ylabel("Average Gradient of Cytosolic calcium (μM)")
-    lgd = fig.legend(lines, labels, title="Number of refinements", loc="center right", bbox_to_anchor=(1.1, 0.5))
+    lgd = fig.legend(
+        lines,
+        labels,
+        title="Number of refinements",
+        loc="center right",
+        bbox_to_anchor=(1.1, 0.5),
+    )
     fig.subplots_adjust(right=0.9)
-    fig.savefig((output_folder / "refinement_study.png").with_suffix(f".{format}"), bbox_extra_artists=(lgd,), bbox_inches="tight")
-        
+    fig.savefig(
+        (output_folder / "refinement_study.png").with_suffix(f".{format}"),
+        bbox_extra_artists=(lgd,),
+        bbox_inches="tight",
+    )
+
+
 def load_timings(folder: Path) -> list[dict[str, Any]]:
     timings = (folder / "timings.txt").read_text()
 
@@ -198,9 +238,9 @@ def load_timings(folder: Path) -> list[dict[str, Any]]:
 
     header = list(map(str.strip, filter(f, timings.splitlines()[0].split("  "))))
     header[0] = "name"
-    
+
     data = []
-    for item_str in timings.splitlines()[2:]:  
+    for item_str in timings.splitlines()[2:]:
         item = list(map(str.strip, filter(f, item_str.split("  "))))
         data.append(dict(zip(header, item)))
 
@@ -209,13 +249,11 @@ def load_timings(folder: Path) -> list[dict[str, Any]]:
     return data
 
 
-
 def load_data(folder: Path = Path("82094")) -> Data:
-
     config_file = folder / "config.json"
     if not config_file.is_file():
         raise FileNotFoundError(config_file)
-    
+
     t_file = folder / "tvec.txt"
     if not t_file.is_file():
         raise FileNotFoundError(t_file)
@@ -224,29 +262,72 @@ def load_data(folder: Path = Path("82094")) -> Data:
     concVec = np.load(folder / "concVec.npy")
     gradVec = np.load(folder / "gradVec.npy")
 
+    if (petsc_timings_file := folder / "petsc_timings.py").is_file():
+        petsc_timings_ = {}
+        exec(petsc_timings_file.read_text(), petsc_timings_)
+        stages = petsc_timings_.pop("Stages")["Main Stage"]
+        petsc_timings = []
+        for k, v in stages.items():
+            petsc_timings.append({"name": k, **v[0]})
+
+    else:
+        petsc_timings = []
+
     config = json.loads(config_file.read_text())
     timings = load_timings(folder=folder)
-    stdout = (
-        folder / f"{folder.name}-dendritic_spine-stdout.txt"
-    ).read_text()
-    stderr = (
-        folder / f"{folder.name}-dendritic_spine-stderr.txt"
-    ).read_text()
-    ntasks = parse_ntasks(stdout=stdout)
+    if (stdout_file := folder / f"{folder.name}-dendritic_spine-stdout.txt").is_file():
+        stdout = stdout_file.read_text()
+        ntasks = parse_ntasks(stdout=stdout)
+    else:
+        stdout = ""
+        ntasks = 1
 
-    return Data(timings_=timings, config=config, t=t, concVec=concVec, gradVec=gradVec, stderr=stderr, stdout=stdout, ntasks=ntasks, folder=folder)
+    if (stderr_file := folder / f"{folder.name}-dendritic_spine-stderr.txt").is_file():
+        stderr = stderr_file.read_text()
+    else:
+        stderr = ""
+
+    return Data(
+        timings_=timings,
+        config=config,
+        t=t,
+        concVec=concVec,
+        gradVec=gradVec,
+        stderr=stderr,
+        stdout=stdout,
+        ntasks=ntasks,
+        folder=folder,
+        petsc_timings_=petsc_timings,
+    )
+
 
 def plot_timings(all_data: list[Data], output_folder, format: str = "png"):
+    data = sorted(
+        [
+            d
+            for d in all_data
+            if "coarser" in d.mesh and np.isclose(d.dt, 0.001) and d.ntasks == 1
+        ],
+        key=lambda x: x.num_refinements,
+    )
 
-    data = sorted([d for d in all_data if "coarser" in d.mesh and np.isclose(d.dt, 0.001) and d.ntasks == 1], key=lambda x: x.num_refinements)
-    
-    keys = ['Initialize Model',
-            "SNES Assemble Jacobian Nested Matrix",
-            'SNES Assemble Residual Nest Vector',
-            "[MixedAssembler] Assemble cells",
-            # "dendritic-spine-example"
-            ]
-    
+    breakpoint()
+
+    keys = [
+        "SNES Assemble Jacobian Nested Matrix",
+        "SNES Assemble Residual Nest Vector",
+        "Initialize Model",
+        # "dendritic-spine-example"
+    ]
+
+    petsc_keys = [
+        "SNESSolve",
+        "KSPSolve",
+        "SnesJacobianEval",
+        "SnesFunctionEval",
+        "PCApply",
+    ]
+
     names = data[0].timings["name"].values.tolist()
 
     # Create bar plot
@@ -256,35 +337,89 @@ def plot_timings(all_data: list[Data], output_folder, format: str = "png"):
         indices.append(names.index(name))
     indices = np.array(indices)
     x = np.arange(0, 3)
-    width = 0.5
+    width = 0.25
     bottom = np.zeros_like(x)
-    breakpoint()
+    # breakpoint()
     for index, key in zip(indices, keys):
         y = np.zeros_like(x)
         for i, d in enumerate(data):
             y[i] = float(d.timings["wall tot"].values[index])
-   
-        # y = d.timings["wall tot"].values[indices].astype(float)
-        # breakpoint()
+
         print(key, y, bottom)
-        ax.bar(x, y, width=width, label=key, bottom=bottom)
+        ax.bar(x - width, y, width=width, label=key, bottom=bottom)
         bottom += y
+
+    index = names.index("[MixedAssembler] Assemble cells")
+    y = [float(d.timings["wall tot"].values[index]) for d in data]
+    ax.bar(x, y, width=width, label="Assemble cells")
+
+    index = names.index("dendritic-spine-example")
+    y = [float(d.timings["wall tot"].values[index]) for d in data]
+    ax.bar(x + width, y, width=width, label="Total run time")
+
     ax.set_xticks(x)
-    # ax.
-    # ax.set_xticklabels(keys, rotation=45)
     ax.legend()
-    # ax.set_ylim(0, 5e5)
-    # ax.set_yscale("log")
-    # fig.tight_layout()
     fig.savefig((output_folder / "timings_profile.png").with_suffix(f".{format}"))
+
+
+def plot_petsc_timings(all_data: list[Data], output_folder, format: str = "png"):
+    data = sorted(
+        [
+            d
+            for d in all_data
+            if "coarser" in d.mesh and np.isclose(d.dt, 0.001) and d.ntasks == 1
+        ],
+        key=lambda x: x.num_refinements,
+    )
+
+    keys = [
+        "KSPSolve",
+        "SNESJacobianEval",
+        "SNESFunctionEval",
+        "PCApply",
+    ]
+
+    names = data[0].petsc_timings["name"].values.tolist()
+
+    # Create bar plot
+    fig, ax = plt.subplots()
+    indices = []
+    for name in keys:
+        indices.append(names.index(name))
+    indices = np.array(indices)
+    x = np.arange(0, 3)
+    width = 0.25
+    bottom = np.zeros_like(x)
     # breakpoint()
-    
-def main(results_folder: Path, output_folder: Path, 
-         format: str = "png",
-         skip_if_processed: bool = False,
-         use_tex: bool = False,
-    ) -> int:
-    
+    for index, key in zip(indices, keys):
+        y = np.zeros_like(x)
+        for i, d in enumerate(data):
+            y[i] = float(d.petsc_timings["time"].values[index])
+
+        print(key, y, bottom)
+        ax.bar(x - width / 2, y, width=width, label=key, bottom=bottom)
+        bottom += y
+
+    index = names.index("SNESSolve")
+    y = [float(d.petsc_timings["time"].values[index]) for d in data]
+    ax.bar(x + width / 2, y, width=width, label="SNESSolve")
+
+    # index = names.index("dendritic-spine-example")
+    # y = [float(d.petsc_timings["time"].values[index]) for d in data]
+    # ax.bar(x + width, y, width=width, label="Total run time")
+
+    ax.set_xticks(x)
+    ax.legend()
+    fig.savefig((output_folder / "petsc_timings_profile.png").with_suffix(f".{format}"))
+
+
+def main(
+    results_folder: Path,
+    output_folder: Path,
+    format: str = "png",
+    skip_if_processed: bool = False,
+    use_tex: bool = False,
+) -> int:
     plt.rcParams["text.usetex"] = use_tex
 
     output_folder.mkdir(exist_ok=True, parents=True)
@@ -303,8 +438,10 @@ def main(results_folder: Path, output_folder: Path,
 
     # plot_data(all_results, output_folder, format=format)
     # plot_refinement_study(all_results, output_folder, format=format)
-    plot_timings(all_results, output_folder, format=format)
+    # plot_timings(all_results, output_folder, format=format)
+    plot_petsc_timings(all_results, output_folder, format=format)
     return 0
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
